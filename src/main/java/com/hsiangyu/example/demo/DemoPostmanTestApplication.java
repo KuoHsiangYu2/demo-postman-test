fd3716ac2a949440d7555038ee41bfe7f2ecbac2
hsiangyukuo <hsiangyugithub@gmail.com>
2024-08-27 16:44:40 +0800

    改成 jdk 1.8環境編譯設定

--- src/main/java/com/hsiangyu/example/demo/DemoPostmanTestApplication.java
+++ src/main/java/com/hsiangyu/example/demo/DemoPostmanTestApplication.java
@@ -8,8 +8,21 @@ public class DemoPostmanTestApplication {
 
     public static void main(String[] args) {
         System.out.println("====start====");
+        System.out.println("中文測試");
+        System.out.println("args");
+        System.out.println(args);
+        System.out.println("args.length = [" + args.length + "]");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i + ". tt " + args[i]);
+        }
         SpringApplication.run(DemoPostmanTestApplication.class, args);
+        System.out.println("中文測試-2");
         System.out.println("====finish====");
     }
 
 }
+
+// mvnw.cmd clean
+// mvnw.cmd install
+// mvnw.cmd spring-boot:run
+// http://127.0.0.1:9191/
